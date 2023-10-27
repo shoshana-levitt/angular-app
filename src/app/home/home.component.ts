fe554f4359fd59052e4bcb887b01a9ffc84b2c1f
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-10-27 15:57:54 -0400

    add json data, use async service method to retrieve data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -45,7 +45,11 @@ export class HomeComponent {
     );
   }
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
-    this.filteredLocationList = this.housingLocationList;
+    this.housingService
+      .getAllHousingLocations()
+      .then((housingLocationList: HousingLocation[]) => {
+        this.housingLocationList = housingLocationList;
+        this.filteredLocationList = housingLocationList;
+      });
   }
 }
