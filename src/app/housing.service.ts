2779a88d490f8e2b592a35ac6050c1226d6b34ef
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-10-27 15:44:41 -0400

    add form, add button with event handler to submit form

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -118,4 +118,9 @@ export class HousingService {
     );
   }
   constructor() {}
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`
+    );
+  }
 }
