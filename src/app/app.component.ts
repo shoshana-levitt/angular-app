5e08983d3c78b147c7c5a972e0777c777a5f7c62
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-10-27 15:35:50 -0400

    add template to DetailsComponent, make Homes logo navigate home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, RouterModule],
+  imports: [HomeComponent, RouterModule, RouterLink, RouterOutlet],
   template: `
     <main>
       <a [routerLink]="['/']">
